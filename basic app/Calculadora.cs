4b2ef3c1cadc7cf59ab59ebf36b5a781f9999ef8
Steven Checo <steven.checo.19@gmail.com>
2020-10-13 19:18:02 -0400

    first commit

---
+++ basic app/Calculadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_app
+{
+    class Calculadora
+    {
+        
+
+        private static void Main(string[] args)
+        {
+            Calculos calculo = new Calculos();
+            var resultado = calculo.calcular(5,10);
+            string cadena = "";
+            cadena = resultado == 15 ? "Verdadero" : "Falso"; 
+            Console.WriteLine("Que tipo es: " + cadena);
+            
+            int f = 0;
+            do
+            {
+                int result = f * 5;
+                Console.WriteLine("{0} x {1} = {2}", 5, f, result);
+                f++;
+            } while (f <= 12);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Calculos {
+
+        public int calcular(int numerouno, int numerodos)
+        {
+            var resultado = numerouno + numerodos;
+            return resultado;
+        }
+    }
+}
